4a0c0633578ac6fee1815663ace884c0d35192fc
Hex-Tron <xredbro@gmail.com>
2021-06-15 23:12:04 +0530

    argparser_added

--- port_checkerV4.py
+++ port_checkerV4.py
@@ -1,17 +1,26 @@
 #time
-#ports
-#threads
-#url
-#timeout
 #color for not connected
 #check for Request per second
 
-
+import argparse
 import threading
 import socket
-host='127.0.0.1'
 
-values=[i for i in range(1,1000)]
+
+parser= argparse.ArgumentParser(description="Port checker")
+parser.add_argument('-u','--url',help="Url Or host",default='127.0.0.1')
+parser.add_argument('-p','--ports',help="how many ports to scan default(1000)",default=1000,type=int)
+parser.add_argument('-t','--threads',help="Total threads to use default(30)",type=int,default=30)
+parser.add_argument('-b','--timeout',help="timeout to inspect a port", default=3,type=int)
+args=parser.parse_args()
+
+
+
+#host='127.0.0.1'
+
+
+
+values=[i for i in range(1,args.ports)]
 values=values[::-1]
 lock=threading.Lock()
 print('---'*10)
@@ -25,10 +34,10 @@ def formater(x):
 
 def target_fuction(i):
     s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    s.settimeout(9)
+    s.settimeout(args.timeout)
     try:
 
-        s.connect((host,i))
+        s.connect((args.url,i))
         #print('\x1b[#F')
         print(f'[+]     \x1b[38;5;46m     connected {i} \x1b[0m',end='\n')
         #print('\x1b[#F')
@@ -62,10 +71,10 @@ def printer():
         target_fuction(x)
 def main():
     global lock
-    threads = [threading.Thread(target=printer) for x in range(30)]
-    for t in threads: 
+    threads_ = [threading.Thread(target=printer) for x in range(args.threads)]
+    for t in threads_: 
         t.start()
-    for t in threads:
+    for t in threads_:
         t.join()
 main()
 print('---'*10)
